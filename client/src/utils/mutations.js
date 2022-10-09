6238dfb166b3fe83d18f7ce976bcd01fbd4117bf
Chris Wallace <Chrisw1096@gmail.com>
2022-10-09 16:28:39 +1100

    app.js set up to work with apollo provider

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,6 @@
 import { gql } from '@apollo/client';
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -28,12 +27,10 @@ export const ADD_USER = gql`
 `;
 
 // SAVE_BOOK will execute the saveBook mutation.
-
 export const SAVE_BOOK = gql`
 mutation saveBook`
 
 // REMOVE_BOOK will execute the removeBook mutation.
-
 export const REMOVE_BOOK = gql`
 mutation removeBook(bookId:ID):[User]
 `
